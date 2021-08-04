d87267977d2fe83a3a4e2968f878248700978b8b
Hani Ho <hani.herbert.ho@gmail.com>
2021-08-04 21:37:30 +0800

    Added AutoMapper. Works both ways.

---
+++ Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public List<string> Items { get; set; }
+    }
+}
---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly MapperConfiguration _mapperConfig;
+        private readonly Mapper _mapper;
+        private Order _order;
+        private OrderDTO _orderDTO;
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            _mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Order, OrderDTO>();
+                cfg.CreateMap<OrderDTO, Order>();
+            });
+            _mapper = new Mapper(_mapperConfig);
+            _order = new Order
+            {
+                Date = DateTime.Now,
+                Id = 1,
+                Name = "Hani",
+                Price = 42,
+                Items = new List<string>()
+                {
+                    "Siomai",
+                    "Pizza"
+                }
+            };
+            _orderDTO = new OrderDTO
+            {
+                Price = 16,
+                Name = "John"
+            };
+        }
+
+        [Route("GetOrder")]
+        [HttpGet]
+        public Order GetOrder()
+        {
+            return _order;
+        }
+
+        [Route("GetOrderDTO")]
+        [HttpGet]
+        public OrderDTO GetOrderDTO()
+        {
+            return _orderDTO;
+        }
+
+        [Route("TransformOrder")]
+        [HttpGet]
+        public OrderDTO TransformOrder()
+        {
+            return _mapper.Map<OrderDTO>(_order);
+        }
+
+        [Route("TransformOrderDTO")]
+        [HttpGet]
+        public Order TransformOrderDTO()
+        {
+            return _mapper.Map<Order>(_orderDTO);
+        }
+    }
+}
